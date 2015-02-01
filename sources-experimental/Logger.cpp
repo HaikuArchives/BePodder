53442cb0e01f27c3af301c007c5fe15ba8dea0da
Augustin Cavalier <waddlesplash@gmail.com>
2015-02-01 14:14:01 -0500

    Fix the build.

--- sources-experimental/Logger.cpp
+++ sources-experimental/Logger.cpp
@@ -22,21 +22,21 @@ check_for_tty()
 {
 	if ( _has_checked_for_tty )
 		return;
-	
+
 	_has_checked_for_tty = true;
-	
+
 	if ( !isatty(STDOUT_FILENO) )
 	{ // redirect output to ~/im_kit.log if not run from Terminal
 		BPath path;
-	
+
 		if (find_directory(B_USER_DIRECTORY,&path,true,NULL) != B_OK)
 			// This should never fail..
 			return;
-		
+
 		path.Append(LogName);
-		
+
 		close(STDOUT_FILENO);
-		open( path.Path(), O_WRONLY|O_CREAT|O_APPEND|O_TEXT);
+		open( path.Path(), O_WRONLY|O_CREAT|O_APPEND);
 		chmod( path.Path(), 0x600 );
 	}
 }
@@ -49,16 +49,16 @@ load_log_level()
 	/*
 	if ( _has_loaded_log_level )
 		return;
-	
+
 	_has_loaded_log_level = true;
-	
+
 	BMessage settings;
 	if ( im_load_client_settings("im_server", &settings) != B_OK )
 	{
 		printf("load_log_level: im_server settings not found\n");
 		return;
 	}
-	
+
 	const char * level=NULL;
 	if ( settings.FindString("log_level", &level) == B_OK )
 	{
@@ -98,30 +98,30 @@ void LOG(const char * module, log_importance level, const char *message, const B
 	va_start(varg, msg);
 	char buffer[2048];
 	vsprintf(buffer, message, varg);
-	
+
 	load_log_level();
-	
+
 	if ( level > liHigh )
 	{
 		level = liHigh;
 	}
-	
+
 	if ( level < g_verbosity_level )
 		return;
-	
+
 	check_for_tty();
-	
+
 	char timestr[64];
 	time_t now = time(NULL);
 	strftime(timestr,sizeof(timestr),"%Y-%m-%d %H:%M", localtime(&now) );
-	
+
 	printf("%s %s (%s): %s\n", module, timestr, gLogLevelText[level], buffer);
 	if ( msg )
 	{
 		printf("BMessage for last message:\n");
 		msg->PrintToStream();
 	}
-	
+
 	fflush(stdout);
 }
 
@@ -130,24 +130,24 @@ void LOG(const char * module, log_importance level, const char *message, ...) {
 	va_start(varg, message);
 	char buffer[2048];
 	vsprintf(buffer, message, varg);
-	
+
 	load_log_level();
-	
+
 	if ( level > liHigh )
 	{
 		level = liHigh;
 	}
-	
+
 	if ( level < g_verbosity_level )
 		return;
-	
+
 	check_for_tty();
-	
+
 	char timestr[64];
 	time_t now = time(NULL);
 	strftime(timestr,sizeof(timestr),"%Y-%m-%d %H:%M", localtime(&now) );
-	
+
 	printf("%s %s (%s): %s\n", module, timestr, gLogLevelText[level], buffer);
-	
+
 	fflush(stdout);
 }
